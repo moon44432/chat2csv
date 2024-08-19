c5fa688b5af294f3a9db3c38c17ee26cd65d010a
Seunggi Moon <moon44432@naver.com>
2024-08-20 00:40:45 +0900

    202408 update

--- chat_extractor.py
+++ chat_extractor.py
@@ -3,26 +3,30 @@ import re
 
 class ChatExtractor:
     def ingame(self, substrings):
-        return ',{0},{1},{2},"{3}"\n' .format("ingame", substrings[0], "", substrings[1])
+        return ',{0},{1},{2},"{3}"\n' .format("ingame", re.sub(self.nickname_star, "", substrings[0]), "", substrings[1])
 
     def dynmap(self, substrings):
-        return ',{0},{1},{2},"{3}"\n' .format("dynmap", substrings[0], "", substrings[1])
+        return ',{0},{1},{2},"{3}"\n' .format("dynmap", re.sub(self.nickname_star, "", substrings[0]), "", substrings[1])
 
     def prompt(self, substrings):
         return ',{0},{1},{2},"{3}"\n' .format("prompt", "", "", substrings[0])
 
     def whisper(self, substrings):
-        return ',{0},{1},{2},"{3}"\n' .format("whisper", substrings[0], substrings[1], substrings[2])
+        return ',{0},{1},{2},"{3}"\n' .format("whisper", re.sub(self.nickname_star, "", substrings[0]), substrings[1], substrings[2])
+    
+    nickname_star = "[☆★]"
 
     patterns_to_remove = [
         r"\[[0-9;]*m",
         r"\bAutoMessage\b"
     ]
+
     patterns_chat_type = {
-        r": <([^,>:]+)> (.+)": ingame,
-        r"\[WEB\] ([^,>:]+): (.+)": dynmap,
+        r": <([^~,>:]+)> (.+)": ingame,
+        r"\[Not Secure\] <([^~,>:]+)> (.+)": ingame,
+        r"\[WEB\] ([^~,>:]+): (.+)": dynmap,
         r"\[Server\] (.+)": prompt,
-        r": ([^,>:]+) issued server command: /tell ([^, ]+) (.+)": whisper,
+        r": ([^~,>:]+) issued server command: /tell ([^, ]+) (.+)": whisper,
     }
 
     def extract_single_line(self, date, line):
