e973d6d36c2add903ab8ed915211f084aa312b3b
Seunggi Moon <moon44432@naver.com>
2025-05-19 14:26:39 +0900

    guess encoding automatically

--- chat2csv.py
+++ chat2csv.py
@@ -17,13 +17,13 @@ def main(args):
     files = get_file_list(log_dir)
 
     csv = open(csv_path, 'w', encoding=args.dest_enc)
-    csv.write('timestamp, type, sender, receiver, content\n')
+    csv.write('server, timestamp, type, sender, receiver, content\n')
 
     extractor = chat_extractor.ChatExtractor(args)
     i, l = 1, len(files)
     for filename in files:
         print("[{0} / {1}] Parsing {2}".format(i, l, filename))
-        for line in extractor.extract(log_dir, filename):
+        for line in extractor.extract(log_dir, filename, args.servername):
             csv.write(line)
         i += 1
 
@@ -33,9 +33,9 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Merge player chat data from Minecraft server logs into a csv file")
 
+    parser.add_argument('--servername', required=True)
     parser.add_argument('--logdir', required=True)
     parser.add_argument('--csvpath', default="output.csv")
-    parser.add_argument('--src-enc', default="utf-8-sig")
     parser.add_argument('--dest-enc', default="utf-8-sig")
 
     args = parser.parse_args()
--- chat_extractor.py
+++ chat_extractor.py
@@ -32,6 +32,12 @@ class ChatExtractor:
         r": ([^~,>:]+) issued server command: /tell ([^, ]+) (.+)": whisper,
     }
 
+    def num_ko_char(self, text):
+        return len(re.findall('[ㄱ-ㅎㅏ-ㅣ가-힣]', text))
+    
+    def num_hanja_char(self, text):
+        return len(re.findall('[\u2e80-\u2eff\u31c0-\u31ef\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fbf\uf900-\ufaff]', text))
+
     def extract_single_line(self, date, line):
         for pattern in self.patterns_to_remove:
             line = re.sub(pattern, '', line)
@@ -42,19 +48,66 @@ class ChatExtractor:
                 return date + ' ' + line[1:9] + self.patterns_chat_type[pattern](self, m.groups())
         return None
 
-    def extract(self, directory, filename):
-        f = open(directory + filename, mode='rb')
-        lines = f.readlines()
+    def extract(self, directory, filename, servername, threshold=20):
+        with open(directory + filename, mode='rb') as f:
+            lines = f.readlines()
 
         str_list = []
 
+        cnt_cp949 = 0
+        cnt_utf8 = 0
+
         for line in lines:
             # Convert encoding from src_enc to dest_enc
-            line = line.decode(self.args.src_enc, errors='ignore').encode(self.args.dest_enc, errors='ignore').decode(self.args.dest_enc)
+            # line = line.decode(self.args.src_enc, errors='ignore').encode(self.args.dest_enc, errors='ignore').decode(self.args.dest_enc)
 
+            line_utf8 = line.decode(encoding='utf-8', errors='ignore')
+            line_euckr = line.decode(encoding='euc-kr', errors='ignore')
+            line_cp949 = line.decode(encoding='cp949', errors='ignore')
+
+            num_ko_utf8 = self.num_ko_char(line_utf8)
+            num_ko_euckr = self.num_ko_char(line_euckr)
+            num_hanja_utf8 = self.num_hanja_char(line_utf8)
+            num_hanja_euckr = self.num_hanja_char(line_euckr)
+
+            if cnt_cp949 > threshold:
+                line = line_cp949
+            elif cnt_utf8 > threshold:
+                line = line_utf8
+            else:
+                if num_ko_utf8 == 0 and num_ko_euckr == 0:
+                    line = line_utf8
+                else:
+                    if num_hanja_utf8 == num_hanja_euckr:
+                        if num_ko_utf8 > num_ko_euckr:
+                            line = line_utf8
+                            cnt_utf8 += 1
+                        elif num_ko_utf8 < num_ko_euckr:
+                            line = line_cp949
+                            cnt_cp949 += 1
+                        else:
+                            print(f"conflict occurred: {line_utf8} | {line_cp949}")
+                            print(f"cnt_cp949: {cnt_cp949}, cnt_utf8: {cnt_utf8}")
+                            if cnt_cp949 >= cnt_utf8:
+                                line = line_cp949
+                            else:
+                                line = line_utf8
+                    elif num_hanja_utf8 > num_hanja_euckr:
+                        line = line_cp949
+                        cnt_cp949 += 1
+                    else:
+                        line = line_utf8
+                        cnt_utf8 += 1
+
+            
+            line = line.replace('\"', '')
+            line = line.replace('\n', '')
+            line = line.replace('\r', '')
+            line = line.replace('쁾', '')
+            line = line.replace('쁿', '')
             string = self.extract_single_line(filename[:10], line)
             if string is not None:
+                string = servername + ',' + string
                 str_list += string
 
-        f.close()
         return str_list
